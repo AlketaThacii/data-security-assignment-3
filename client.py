3e4f753e25e62b5a88632d783ea2af758c22b8fc
almajashari <alma.jashari2@student.uni-pr.edu>
2026-05-08 11:33:07 +0200

    permiresim i rendeve te kodit

--- client.py
+++ client.py
@@ -9,18 +9,7 @@ from Crypto.Random import get_random_bytes
 hostname = " "
 portno = 0000
 
-try:
-    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print("Socketi eshte gati i gjeneruar")
-
-    mySocket.connect((hostname, portno))
-    print("Lidhja eshte arritur me serverin")
-
-except:
-    print("Nuk mund te lidhet me serverin")
-    exit()
-
-    def encrypt_des(message, des_key):
+  def encrypt_des(message, des_key):
     iv = get_random_bytes(8)
     cipher = DES.new(des_key, DES.MODE_CBC, iv)
     encrypted = cipher.encrypt(pad(message.encode("utf-8"), DES.block_size))
@@ -33,6 +22,18 @@ def decrypt_des(encrypted_data, des_key):
     cipher = DES.new(des_key, DES.MODE_CBC, iv)
     decrypted = unpad(cipher.decrypt(ciphertext), DES.block_size)
     return decrypted.decode("utf-8")
+try:
+    mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("Socketi eshte gati i gjeneruar")
+
+    mySocket.connect((hostname, portno))
+    print("Lidhja eshte arritur me serverin")
+
+except:
+    print("Nuk mund te lidhet me serverin")
+    exit()
+
+  
 
 public_key_pem = mySocket.recv(4096)
 public_key = RSA.import_key(public_key_pem)
@@ -63,3 +64,6 @@ if "roli: admin" in msg_in.lower():
     role = "admin"
 else:
     role = "read-only"
+
+
+
